24878997cca6e5c260bcab7940a171b0b4f30c9f
robguevara <robguevara@paypal.com>
2019-09-12 08:25:27 -0700

    Declaring a Winner

--- tic-tac-toe/components/Board.js
+++ tic-tac-toe/components/Board.js
@@ -5,6 +5,26 @@ import {
 } from '../../todomvc/lib/state-manager';
 import Square from './Square';
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i += 1) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 // Board
 // <div>
 //   <div class="status">status</div>
@@ -33,6 +53,9 @@ function Board() {
   function view(state) {
     function handleOnClick(i) {
       const squares = state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       squares[i] = state.xIsNext ? 'X' : 'O';
       store.dispatch(
         {
@@ -54,7 +77,13 @@ function Board() {
       });
     }
 
-    const status = `Next player: ${state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${state.xIsNext ? 'X' : 'O'}`;
+    }
 
     return d(
       'div',
